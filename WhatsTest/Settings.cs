a5a185d158bfcfa6eb7b3aacc6cc2016c42b3dc7
Ruud Vriens <ruudvriens@hotmail.com>
2016-06-21 22:51:45 +0200

    Settings

---
+++ WhatsTest/Settings.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WhatsTest
+{
+    [Serializable()]
+    public class Settings
+    {
+        public string Nickname;
+        public string Sender;
+        public string Password;
+        public string Target;
+    }
+}
